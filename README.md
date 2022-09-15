fad56a037e94f86247d2793619f3c3196f8bfc3b
ausbennett <61357467+ausbennett@users.noreply.github.com>
2022-09-15 15:18:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-xqwd5b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-xqwd5b)
---
+++ src/home/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <span class="hello">Hello</span>
+</template>
+<script>
+export default { name: 'HelloWorld' };
+</script>
+<style></style>
